affcacd3e84ac6e86ffd3c66f87728982fe19d81
juicerq <julio.cerqueiira@gmail.com>
2026-08-24 13:53:43 -0300

    keep BANKAI_DAEMON out of the test environment

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,10 +2,12 @@ import { mkdtempSync, rmSync } from "node:fs";
 import { tmpdir } from "node:os";
 import { join } from "node:path";
 import { afterEach, beforeEach } from "bun:test";
+import { DAEMON_ENV_FLAG } from "@shared/daemon";
 import "./utils/electron-mock";
 
 beforeEach(() => {
 	process.env.DATA_DIR = mkdtempSync(join(tmpdir(), "bankai-test-"));
+	delete process.env[DAEMON_ENV_FLAG];
 });
 
 afterEach(() => {
